b8d587bb208ee42e3de96de899c791a7d9104e41
madiyar <MaBolatov@dar.kz>
2020-10-18 17:56:23 +0600

    mui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
+import configureStore from 'store';
+import theme from 'styles';
 
 function App() {
   return (
-    <div className="App">HELLO</div>
+    <Provider store={configureStore()}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          <Switch>
+            <Route path="/" exact>HELLO</Route>
+          </Switch>
+        </Router>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
