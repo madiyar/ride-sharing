411c2bfc02802e6f6cbe6ef34cb5944f4e02fc8c
madiyar <MaBolatov@dar.kz>
2020-10-18 17:31:26 +0600

    firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDvo4Je1g1TC1Xgt6iuf2HL5ya8A-3R8kw",
+  authDomain: "kolik-50a2d.firebaseapp.com",
+  databaseURL: "https://kolik-50a2d.firebaseio.com",
+  projectId: "kolik-50a2d",
+  storageBucket: "kolik-50a2d.appspot.com",
+  messagingSenderId: "238880769541",
+  appId: "1:238880769541:web:53e5f36301c88b4800fe6c"
+});
 
 ReactDOM.render(
   <React.StrictMode>
