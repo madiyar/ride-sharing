fc94e1e276ce1089ce6a05010224ba66bcdb9395
madiyar <MaBolatov@dar.kz>
2020-10-18 18:32:33 +0600

    icons

---
+++ src/components/Icon/index.js
@@ -0,0 +1,2 @@
+import * as Icon from 'react-feather';
+export default Icon;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { Icon } from 'components';
 
 const Pages = () => {
   return (
     <Switch>
       <Route path="/" exact>
-        HELLO
+        <Icon.Camera />
       </Route>
     </Switch>
   )
